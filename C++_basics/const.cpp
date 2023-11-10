9c70e0d0350462f193ded8f3365e919348b0a7ac
Keshu <2003013053@ipec.org.in>
2023-11-10 11:06:32 +0530

    const keyword

---
+++ C++_basics/const.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int x = 10;//const variable cannot be updated except using pointer
+    // x++;
+    cout<<x<<endl;
+    cout<<x<<endl;
+    cout<<x<<endl;
+}
